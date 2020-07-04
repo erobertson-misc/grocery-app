45438193fcc01175ea1e884329ad377ecd503ef5
eric-robertson <e.robertson.js@gmail.com>
2020-07-04 19:30:41 -0400

    removed removed files

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Eric Robertson\Documents\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Eric Robertson\Documents\Projects\Personal2\misc\grocery-app\grocery_app2"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Eric Robertson\Documents\Flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
